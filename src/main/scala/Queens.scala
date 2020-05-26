3b1c69cb4ae4f8a4cc176dab01f0cd03b411798a
Roland Tritsch <roland@tritsch.org>
2020-05-26 08:05:29 +0100

    Using scalafmt (for the first time).

--- src/main/scala/Queens.scala
+++ src/main/scala/Queens.scala
@@ -15,6 +15,7 @@ import com.typesafe.scalalogging.LazyLogging
 
 /** Main class/object to solve the N Queens problem. */
 object Queens extends LazyLogging {
+
   /** Describe a position on the board. Also checks, if putting another Queens into position p is legal. */
   case class Pos(row: Int, column: Int) {
     def sameRow(p: Pos) = row == p.row
@@ -25,19 +26,22 @@ object Queens extends LazyLogging {
   }
 
   /** @return a row as a list of positions. */
-  def rowList(size: Int, row: Int) = (0 until size).toList.map(column => Pos(row, column))
+  def rowList(size: Int, row: Int) =
+    (0 until size).toList.map(column => Pos(row, column))
 
   /** @return a list of possible solutions with the first queen placed on each. */
   def seed(size: Int) = rowList(size, 0).map(sol => List(sol))
 
   /** @return expand the list of existing solutions with one more queen and look for legal positions. */
   def expand(solutions: List[List[Pos]], size: Int, row: Int) = {
-    logger.debug(size + "/" + row + "/" + solutions.size + "/" + solutions(0).size)
+    logger.debug(
+      size + "/" + row + "/" + solutions.size + "/" + solutions(0).size
+    )
     logger.trace("" + solutions)
     (for {
       solution <- solutions
       pos <- rowList(size, row)
-      if(solution.forall(_.legal(pos)))
+      if (solution.forall(_.legal(pos)))
     } yield pos :: solution).toList
   }
 
@@ -47,7 +51,7 @@ object Queens extends LazyLogging {
   /** @return a string that shows/dumps a board/solution (for debugging). */
   def dump(s: List[Pos]): String = {
     val board = Array.fill(s.size)(Array.fill(s.size)("."))
-    s.map(p => p match {case Pos(r, c) => board(r)(c) = "Q"})
+    s.map(p => p match { case Pos(r, c) => board(r)(c) = "Q" })
     board.map(_.mkString(" ")).mkString("\n")
   }
 
